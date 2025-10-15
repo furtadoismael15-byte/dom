0d9d9e0e77e7520d8d195fc0bfe76433ebd02668
furtadoismael15-byte <furtadoismael15@gmail.com>
2025-10-15 13:36:56 -0300

    dom
    dom

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+// Mulher
+document.getElementById("btnMulher").addEventListener("click", function () {
+    document.getElementById("header").style.backgroundColor = "pink";
+}, false)
+
+
+
+// Homem
+document.getElementById("btnHomem").addEventListener("click", function () {
+    document.getElementById("header").style.backgroundColor = "green";
+}, false)
